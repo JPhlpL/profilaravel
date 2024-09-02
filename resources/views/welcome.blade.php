f6bfbc3f21dd420332824db5936e666cc369b701
John Philip Lominoque <johnphilipomamalin1206@gmail.com>
2024-09-02 22:31:19 +0800

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,82 @@
+<!--
+	Thanks -->
+    <!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<meta name="viewport" content="width=device-width,initial-scale=1.0">
+		<link rel="stylesheet" href="{{ URL::asset('css/style.css');}}">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+		<link rel="icon" href="Icons/prof.png">
+		<title>John Philip Lominoque - Profile</title>
+	</head>
+	<body id="backgroundpic">
+		<header>
+			<div class="menu-btn">
+				<div class="linebutton"></div>
+				<div class="linebutton"></div>
+				<div class="linebutton"></div>
+			</div>
+			<nav class="menu">
+				<div class="menu-branding">
+					<div class="profpic"></div>
+				</div>
+				<ul class="menu-nav">
+					<li class="nav-item current">
+						<a href="index.html" class="nav-link">
+							Home
+						</a>
+					</li>
+					<li class="nav-item">
+						<a href="Web/tng.html" class="nav-link">
+							About
+						</a>
+					</li>
+					<li class="nav-item">
+						<a href="Web/trb.html" class="nav-link">
+							Projects
+						</a>
+					</li>
+					<li class="nav-item">
+						<a href="Web/skillset.html" class="nav-link">
+							Skillset
+						</a>
+					</li>
+					<li class="nav-item">
+						<a href="Web/ctt.html" class="nav-link">
+							Contact
+						</a>
+					</li>
+				</ul>
+			</nav>
+		</header>
+		<main id="local">
+			<h1 class="headr">
+			<span class="text-secondary">John</span> Philip
+			</h1>
+			<h2 class="subheadr">
+				CS Grad, Programmer & <span class="text-secondary">Technophile</span>
+			</h2>
+			<div class="ico">
+				<a href="https://facebook.com/Lipip.JP" target="_blank">
+					<i class="fab fa-facebook fa-2x"></i>
+				</a>
+				<a href="https://github.com/JPhlpL" target="_blank">
+					<i class="fab fa-github fa-2x"></i>
+
+				<a href="https://www.linkedin.com/in/john-philip-lominoque-787814189/" target="_blank">
+					<i class="fab fa-linkedin fa-2x"></i>
+				</a>
+				<a href="https://stackoverflow.com/users/9652880/john-philip" onclick="this.target='_blank'">
+					<i class="fab fa-stack-overflow fa-2x"></i>
+				</a>
+			</div>
+			</main>
+		<script src="JS/hov.js"></script>
+	</body>
+</html>
+
+
+
+
